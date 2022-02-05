6576df3536c274e821e28b510d0aa68daba5d459
DIEGOHORVATTI <susanomonkey@gmail.com>
2022-02-05 18:28:16 -0400

    atualização

---
+++ 6_handlebars/A2_dados_views/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const port = 3000
+
+const app = express()
+app.engine('handlebars', exphbs.engine()) 
+
+app.set('view engine', 'handlebars')
+
+
+app.get( '/', (req, res) => {
+  res.render('home')
+})
+
+app.use((req, res, next) => {
+  //res.status(404).sendFile(`${basePath}/404.html`)
+  res.status(404).send('Erro arquivo não encontrato - 404!')
+  res.status(500).send('Erro servidor - 500!')
+})
+
+app.listen(port, () => {
+  console.log(`App rodando na porta:${port}`)
+})
---
+++ 6_handlebars/A3_layouts/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const port = 3000
+
+const app = express()
+app.engine('handlebars', exphbs.engine()) 
+
+app.set('view engine', 'handlebars')
+
+
+app.get( '/', (req, res) => {
+
+  const user = {
+    name: "Diego",
+    apelido: "Div",
+    age: 19
+  }
+
+  res.render('home', { user: user })
+})
+
+app.use((req, res, next) => {
+  //res.status(404).sendFile(`${basePath}/404.html`)
+  res.status(404).send('Erro arquivo não encontrato - 404!')
+  res.status(500).send('Erro servidor - 500!')
+})
+
+app.listen(port, () => {
+  console.log(`App rodando na porta:${port}`)
+})
