968b839a7a587a8d86b7fa344f702abd5bd569ee
DIEGOHORVATTI <susanomonkey@gmail.com>
2022-02-26 23:38:18 -0400

    fix: estudos sql

---
+++ 7_node_e_mysql/1_intalando_mysql/controllers/index.js
@@ -0,0 +1,107 @@
+//"chalk": "^3.0.0",
+//"express-handlebars": "^5.3.3"
+//"boxen": "^5.1.2"
+
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mysql = require('mysql')
+const boxen = require('boxen')
+const chalk = require('chalk')
+
+const app = express()
+const port = 3000
+
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+)
+
+app.post('/books/insertbook', (req, res) => {
+  const now = new Date()
+  const dia = now.getDate()
+  const mes = now.getMonth() + 1
+  const ano = now.getFullYear()
+  const horas = now.getHours()
+  const minutos = now.getMinutes()
+
+  const title = req.body.title
+  const pages = req.body.pages
+
+  // eslint-disable-next-line quotes
+  /* ${ano}-${mes}-${dia} */
+  const sql = `INSERT INTO Books (title, pages) VALUES ( '${title}', '${pages}')`
+
+  conn.query(sql, function (err) {
+    if (err) {
+      console.log(
+        boxen(
+          `
+Erro no sql: ${chalk.red(err)}s
+
+${chalk.green.bgCyan('Conectado ao MySQL!')}
+          `,
+          {
+            margin: 1,
+            float: 'center',
+            padding: 1,
+            borderStyle: 'round',
+            borderColor: 'green',
+          }
+        )
+      )
+    }
+
+    res.redirect('/')
+  })
+})
+
+/* ----- */
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'div',
+  password: '20020000',
+  database: 'nodemysql_2',
+})
+
+conn.connect((err) => {
+  if (err) {
+    console.log(
+      boxen(`Erro de conexão no banco: ${chalk.red(err)}`, {
+        margin: 1,
+        float: 'center',
+        padding: 1,
+        borderStyle: 'round',
+        borderColor: 'green',
+      })
+    )
+  }
+})
+
+app.listen(port, () => {
+  console.log(
+    boxen(
+      `
+Aberto na porta ${chalk.red(port)}
+
+${chalk.green.bgCyan('Conectado ao MySQL!')}
+          `,
+      {
+        margin: 1,
+        float: 'center',
+        padding: 1,
+        borderStyle: 'round',
+        borderColor: 'green',
+      }
+    )
+  )
+})
