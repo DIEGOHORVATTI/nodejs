55a5e69ba26446ff59f00418a4a6f995f53a5477
DIEGOHORVATTI <susanomonkey@gmail.com>
2022-03-09 07:10:53 -0400

    v0.0.2

--- 8_sequelize/2_model/db/conn.js
+++ 8_sequelize/2_model/db/conn.js
@@ -7,27 +7,4 @@ const sequelize = new Sequelize('sequelize', 'div', '20020000', {
   dialect: 'mysql',
 })
 
-/* try{
-  sequelize.authenticate()
-  console.log(
-    boxen('Conectado ao sql|(sequelize)', {
-      margin: 1,
-      float: 'center',
-      padding: 1,
-      borderStyle: 'round',
-      borderColor: 'green',
-    })
-  )
-} catch(err){
-  console.log(
-    boxen(`Erro no sql|(conn.js): ${chalk.red(err)}`, {
-      margin: 1,
-      float: 'center',
-      padding: 1,
-      borderStyle: 'round',
-      borderColor: 'green',
-    })
-  )
-} */
-
 module.exports = sequelize
